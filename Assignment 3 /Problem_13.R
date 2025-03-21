a3ea2fe4cbc55414fd58812e81de092e61b524f1
Yash shukla <newyashshukla15@gmail.com>
2025-03-21 23:01:59 +0530

    Update Problem_13.R

--- Assignment 3 /Problem_13.R
+++ Assignment 3 /Problem_13.R
@@ -1,1 +1,6 @@
 
+x <- factor(c("Jan", "Mar", "Feb", "Apr", "May", "Dec", "Nov"),
+            levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                       "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
+            ordered = TRUE)
+print(x)
