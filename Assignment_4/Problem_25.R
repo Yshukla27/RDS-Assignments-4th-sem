ca713f53393a8ea74ceafadf36353ffddbd422a7
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 13:04:09 +0530

    Update Problem_25.R

--- Assignment_4/Problem_25.R
+++ Assignment_4/Problem_25.R
@@ -1,1 +1,10 @@
-
+#Write R code that dynamically adds named elements to an existing list based on user input.
+my_list <- list()
+element_name <- readline(prompt = "Enter the name of the new element: ")
+element_value <- readline(prompt = "Enter the value of the new element: ")
+element_value <- as.numeric(element_value)
+my_list[[element_name]] <- element_value
+print(my_list)
+#Output
+# [[1]]
+# [1] NA
