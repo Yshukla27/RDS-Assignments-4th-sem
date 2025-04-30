2b103aa9c505edf004316c74a655d745d6b085b2
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 12:19:18 +0530

    Create Problem_1.R

---
+++ Assignment_4/Problem_1.R
@@ -0,0 +1,13 @@
+#Create a list containing a number, a string, and a logical value.
+ my_list <- list(42, "Hello", TRUE)
+ my_list
+ 
+ # Output 
+ # [[1]]
+ #  [1] 42
+ 
+ # [[2]]
+ # [1] "Hello"
+ 
+ # [[3]]
+ # [1] TRUE
