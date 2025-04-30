9741efd13e56bef7201e1405a0c70d33975fbf0e
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 12:58:36 +0530

    Update Problem_10.R

--- Assignment_4/Problem_10.R
+++ Assignment_4/Problem_10.R
@@ -1,1 +1,9 @@
-
+#Add a new row to an existing data frame.
+my_data <- rbind(my_data, data.frame(Name = "David", Age = 28))
+my_data
+#Output
+#      Name Age
+# 1   Alice  25
+# 2     Bob  30
+# 3 Charlie  22
+# 4   David  28
