46c8da19c272fe08c9809bf8a0255f959806f066
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 13:02:18 +0530

    Update Problem_19.R

--- Assignment_4/Problem_19.R
+++ Assignment_4/Problem_19.R
@@ -1,1 +1,13 @@
-
+#How do you subset a data frame to exclude a particular column?
+my_data <- data.frame(
+  Name = c("Alice", "Bob", "Charlie"),
+  Age = c(25, 30, 22),
+  City = c("New York", "Los Angeles", "Chicago")
+)
+my_data_excluded <- my_data[, -2]
+print(my_data_excluded)
+#Output
+#     Name        City
+# 1   Alice    New York
+# 2   Bob     Los Angeles
+# 3  Charlie     Chicago
