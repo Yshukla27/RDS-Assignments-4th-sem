8169200e6b8b9bcd707272f8431e1c4f6f78b8d2
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 13:00:24 +0530

    Update Problem_14.R

--- Assignment_4/Problem_14.R
+++ Assignment_4/Problem_14.R
@@ -1,1 +1,9 @@
-
+#Create a logical subset of a data frame to include only rows where age > 20.
+subset_data <- my_data[my_data$Age > 20, ]
+subset_data
+#Output
+#      Name Age
+# 1   Alice  25
+# 2     Bob  30
+# 3 Charlie  22
+# 4   David  28
