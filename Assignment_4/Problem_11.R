efbdb77ef409e35b056472c56027877d5f6023b5
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 12:58:56 +0530

    Update Problem_11.R

--- Assignment_4/Problem_11.R
+++ Assignment_4/Problem_11.R
@@ -1,1 +1,19 @@
+#Create a nested list where one element is itself a list containing a numeric vector and a
+#character vector.
+nested_list <- list(
+  name = "Outer List",inner_list = list(
+    numeric_vector = c(1, 2, 3, 4),
+    char_vector = c("apple", "banana", "cherry")
+  )
+)
+nested_list
+#Output
+# $name
+# [1] "Outer List"
 
+# $inner_list
+# $inner_list$numeric_vector
+# [1] 1 2 3 4
+
+# $inner_list$char_vector
+# [1] "apple"  "banana" "cherry"
