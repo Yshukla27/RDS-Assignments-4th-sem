c3698528c84464140497092639e277f0996fdd72
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 13:03:52 +0530

    Update Problem_24.R

--- Assignment_4/Problem_24.R
+++ Assignment_4/Problem_24.R
@@ -1,1 +1,10 @@
+#Create a list containing a matrix, a logical vector, and a string. Then, extract the second element of the logical vector.
+my_matrix <- matrix(1:6, nrow = 2, ncol = 3)
+my_logical_vector <- c(TRUE, FALSE, TRUE)
+my_string <- "Hello, World!"
 
+my_list <- list(my_matrix, my_logical_vector, my_string)
+second_logical_element <- my_list[[2]][2]
+print(second_logical_element)
+#Output
+#[1] FALSE
