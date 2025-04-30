e44eb426108a47d89fe7162c4658b863413a05bc
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 13:05:01 +0530

    Update Problem_28.R

--- Assignment_4/Problem_28.R
+++ Assignment_4/Problem_28.R
@@ -1,1 +1,10 @@
-
+#Demonstrate how to subset a nested list to extract a deeply nested numeric value.
+my_list <- list(
+  first_level = list(
+    second_level = list(
+      third_level = list(
+        numeric_value = 42))))
+numeric_value <- my_list$first_level$second_level$third_level$numeric_value
+print(numeric_value)
+#Output
+#[1] 42
