b8ef2bd1a0f3d2a7f9006da93d61bbe2f4b0ed69
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 12:56:05 +0530

    Update Problem_5.R

--- Assignment_4/Problem_5.R
+++ Assignment_4/Problem_5.R
@@ -1,1 +1,12 @@
+#Name the elements of a list explicitly.
+my_list <- list(number = 42, text = "Hello", logic = TRUE)
+my_list
+# Output
+# $number
+# [1] 42
 
+# $text
+# [1] "Hello"
+
+# $logic
+# [1] TRUE
