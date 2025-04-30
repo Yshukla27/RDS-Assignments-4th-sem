22e2636e9184b873d7485c78de814f298b06457d
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 13:02:55 +0530

    Update Problem_21.R

--- Assignment_4/Problem_21.R
+++ Assignment_4/Problem_21.R
@@ -1,1 +1,24 @@
+#Write a function to merge two lists into one nested list without losing the original list structure.
+merge_lists <- function(list1, list2) {
+  nested_list <- list(list1, list2)
+   return(nested_list)
+}
+list1 <- list(Name = "Alice", Age = 25)
+list2 <- list(City = "New York", Occupation = "Engineer")
+result <- merge_lists(list1, list2)
+print(result)
+#Output
+# [[1]]
+# [[1]]$Name
+# [1] "Alice"
 
+# [[1]]$Age
+# [1] 25
+
+
+# [[2]]
+# [[2]]$City
+# [1] "New York"
+
+# [[2]]$Occupation
+# [1] "Engineer
