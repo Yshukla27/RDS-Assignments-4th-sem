a8e54ca591d277ac29cbacc30b72143b4197ac1e
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 13:01:38 +0530

    Update Problem_17.R

--- Assignment_4/Problem_17.R
+++ Assignment_4/Problem_17.R
@@ -1,1 +1,15 @@
+#Demonstrate the difference between single bracket [ ] slicing and double bracket [[ ]] referencing with a suitable example.
+my_list <- list(
+  number = 42,text = "Hello",
+  logic = TRUE
+)
+my_lsit
+#Output
+# $number
+# [1] 42
 
+# $text
+# [1] "Hello"
+
+# $logic
+# [1] TRUE
