b9fa2e359b0b9fbdf4dc1af3da023e980628bd48
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 13:03:15 +0530

    Update Problem_22.R

--- Assignment_4/Problem_22.R
+++ Assignment_4/Problem_22.R
@@ -1,1 +1,15 @@
-
+#Create a data frame that recycles shorter vectors. Demonstrate and explain the behavior.
+name_vector <- c("Alice", "Bob", "Charlie")   
+age_vector <- c(25, 30)                       
+city_vector <- c("New York", "Los Angeles")    
+my_data <- data.frame(
+  Name = name_vector,
+  Age = age_vector,
+  City = city_vector
+)
+print(my_data)
+#Output
+#      Name  Age      City
+# 1   Alice  25     New York
+# 2     Bob  30 Los Angeles
+# 3 Charlie  25     New York
