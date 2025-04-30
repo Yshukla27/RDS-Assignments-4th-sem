69d9ab71059385fc0934f01047c207acd294c4c6
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 13:05:33 +0530

    Update Problem_30.R

--- Assignment_4/Problem_30.R
+++ Assignment_4/Problem_30.R
@@ -1,1 +1,12 @@
-
+#Using logical vectors, subset a data frame to extract rows based on multiple conditions across different columns.
+my_data <- data.frame(
+  Name = c("Alice", "Bob", "Charlie", "David"),
+  Age = c(25, 30, 35, 40),
+  Height = c(5.5, 6.0, 5.8, 5.9)
+)
+subset_data <- my_data[my_data$Age > 30 & my_data$Height > 5.7, ]
+print(subset_data)
+#Output
+#    Name   Age  Height
+# 3 Charlie  35    5.8
+# 4  David   40    5.9
