a969da0aa602995c9e51d255b95e7dd9fedcbfc1
Yash shukla <newyashshukla15@gmail.com>
2025-04-30 12:56:39 +0530

    Update Problem_7.R

--- Assignment_4/Problem_7.R
+++ Assignment_4/Problem_7.R
@@ -1,1 +1,11 @@
-
+#Create a simple data frame with two columns: Name and Age.
+my_data <- data.frame(
+  Name = c("Alice", "Bob", "Charlie"),
+  Age = c(25, 30, 22)
+)
+my_data
+# Output
+#      Name Age
+# 1   Alice  25
+# 2     Bob  30
+# 3 Charlie  22
